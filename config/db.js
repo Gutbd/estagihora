01984996cfe9e358ced51513ef6e8cb8569bf080
Gut <gustavobd@gmail.com>
2021-01-12 14:17:50 -0300

    Create initial server configuration

---
+++ config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const dataBaseURI = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    // Connect to Mongo with Mongoose
+    mongoose
+      .connect(dataBaseURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      })
+      .then(() => console.log('Mongo connected'))
+      .catch((err) => console.log(err));
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;

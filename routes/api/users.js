d1d0d7e348ee355345de5850d1859e2b9dd5fed4
Gut <gustavobd@gmail.com>
2021-01-12 16:34:28 -0300

    Create users GET route endpoint

---
+++ routes/api/users.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+
+// @route   POST api/users
+// @desc    Register a user
+// @access  Public
+router.post(
+  '/',
+  [
+    check('name', 'Nome é obrigatório').not().isEmpty(),
+    check('email', 'Utilize um e-mail válido')
+      .isEmail()
+      .custom((email) => User.isUniqueEmail(email)),
+    check('password', 'Sua senha deve ter no mínimo 6 caracteres').isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      const user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // save user
+      await user.save();
+
+      // jwt
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: config.get('jwtExpiresIn') },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+      res.status(200).json({ msg: 'Usuário cadastrado com sucesso!' });
+    } catch (error) {
+      res.status(500).json({ errors: [{ msg: error.message }] });
+    }
+  }
+);
+
+module.exports = router;

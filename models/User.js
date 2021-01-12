130a98d766e6c2ffad3262accbe4c5287194b749
Gut <gustavobd@gmail.com>
2021-01-12 16:32:43 -0300

    Create a User model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  // Name
+  name: {
+    type: String,
+    required: true,
+  },
+
+  // Email
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  // Password
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Email must be unique
+UserSchema.statics = {
+  isUniqueEmail(email) {
+    return this.findOne({ email }).then((result) => {
+      if (result) throw new Error('E-mail já cadastrado');
+    });
+  },
+};
+
+module.exports = User = mongoose.model('User', UserSchema);

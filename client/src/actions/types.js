573b979151b12bd6e2eb57f3fed3635041eea1c2
Gut <gustavobd@gmail.com>
2021-01-15 09:34:59 -0300

    Create types to action files

---
+++ client/src/actions/types.js
@@ -0,0 +1,21 @@
+// -------- Registration
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+
+// -------- Authentication / Login
+export const AUTH_ERROR = "AUTH_ERROR";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT = "LOGOUT";
+
+// -------- Toggle Login / register
+export const toggle_CONNECT = "toggle_CONNECT";
+
+// -------- Load User
+export const USER_LOADED = "USER_LOADED";
+
+// -------- Checkpoint
+export const CHECKPOINT_SEND_SUCCESS = "CHECKPOINT_SEND_SUCCESS";
+export const CHECKPOINT_SEND_FAIL = "CHECKPOINT_SEND_FAIL";
+export const CHECKPOINT_LOAD_SUCCESS = "CHECKPOINT_LOAD_SUCCESS";
+export const CHECKPOINT_LOAD_FAIL = "CHECKPOINT_LOAD_FAIL";

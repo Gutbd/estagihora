e6e3463304f7e6a2272570de55e72a2ee8f9f5b3
Gut <gustavobd@gmail.com>
2021-01-15 09:38:31 -0300

    Create services

---
+++ client/src/services/AuthService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+// Method to set token inside header before requisitions
+const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common["x-auth-token"] = token;
+  } else {
+    delete axios.defaults.headers.common["x-auth-token"];
+  }
+};
+
+export default setAuthToken;

0180afaa3adf5aba09001236cc66214dcd2960b2
Gut <gustavobd@gmail.com>
2021-01-17 17:09:52 -0300

    Fix path names of new components

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import PrivateRoute from "../routing/PrivateRoute";
+import PrivateRoute from '../routing/PrivateRoute';
 
-import Dashboard from "../dashboard/Dashboard";
-import History from "../history/History";
-import NotFound from "../layout/NotFound";
+import History from '../../pages/history/History';
+import NotFound from '../layout/NotFound';
+import Home from '../../pages/home/Home';
 
 const Routes = () => {
   return (
     <section>
       <Switch>
-        <PrivateRoute exact path='/home' component={Dashboard} />
+        <PrivateRoute exact path='/home' component={Home} />
         <PrivateRoute exact path='/history' component={History} />
         <Route component={NotFound} />
       </Switch>

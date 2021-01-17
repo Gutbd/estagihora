9406cdfa685746162ac35657f5206e6be63049f4
Gut <gustavobd@gmail.com>
2021-01-17 17:06:57 -0300

    Refactor component with better redux usage

--- client/src/components/checkpoint/CheckpointInfo.js
+++ client/src/components/checkpoint/CheckpointInfo.js
@@ -1,11 +1,13 @@
-import React, { Fragment } from "react";
-import { connect } from "react-redux";
+import React, { Fragment } from 'react';
+import { connect, useSelector } from 'react-redux';
 
-import { hasCheckpointPendentDay } from "../../utils/checkpointUtils";
+import { hasCheckpointPendentDay } from '../../utils/checkpointUtils';
 
-import AlertMessage from "../layout/AlertMessage";
+import AlertMessage from '../layout/AlertMessage';
 
-const CheckpointInfo = ({ checkpointList }) => {
+const CheckpointInfo = () => {
+  const { checkpointList } = useSelector((state) => state.checkpoint);
+
   return (
     <Fragment>
       {hasCheckpointPendentDay(checkpointList) && (
